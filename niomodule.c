a643610fe0734e90212c6602f19fb33dbe9152f7
haley <haley@10fe2890-8a40-4696-8b1d-7ccbb19c398e>
2007-05-21 14:40:14 +0000

    Added:
    /* Py_ssize_t for old Pythons */
    /* This code is as recommended by: */
    /* http://www.python.org/dev/peps/pep-0353/#conversion-guidelines */
    #if PY_VERSION_HEX < 0x02050000 && !defined(PY_SSIZE_T_MIN)
    typedef int Py_ssize_t;
    # define PY_SSIZE_T_MAX INT_MAX
    # define PY_SSIZE_T_MIN INT_MIN
    #endif
    
    so that PyNIO will build under Python 2.4
    
    --Mary

--- niomodule.c
+++ niomodule.c
@@ -24,6 +24,14 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+/* Py_ssize_t for old Pythons */
+/* This code is as recommended by: */
+/* http://www.python.org/dev/peps/pep-0353/#conversion-guidelines */
+#if PY_VERSION_HEX < 0x02050000 && !defined(PY_SSIZE_T_MIN)
+typedef int Py_ssize_t;
+# define PY_SSIZE_T_MAX INT_MAX
+# define PY_SSIZE_T_MIN INT_MIN
+#endif
 
 #define _NIO_MODULE
 #include "niomodule.h"
