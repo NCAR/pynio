02dbd476cea865e75c5535a7cf382218e3a05725
haley <haley@10fe2890-8a40-4696-8b1d-7ccbb19c398e>
2010-02-25 17:04:26 +0000

    Added an ldd/otool command for extra testing.

--- test/test-version.py
+++ test/test-version.py
@@ -1,6 +1,16 @@
-import Nio, os
+import Nio, os, sys
+from distutils.sysconfig import get_python_lib
 
 print "Nio version = '"+Nio.__version__+"'"
 print "Nio path = '"+Nio.__file__+"'"
 
 os.system("ls -l " + Nio.__file__)
+
+plib = get_python_lib()
+
+
+if sys.platform == "darwin":
+  os.system("otool -L " + plib + "/PyNIO/" + "nio.so")
+else:
+  os.system("ldd " + plib + "/PyNIO/" + "nio.so")
+
