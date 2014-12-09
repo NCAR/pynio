666d09dff6e6ed667b3ea479fc62a05690681840
dbrown <dbrown@10fe2890-8a40-4696-8b1d-7ccbb19c398e>
2014-12-09 00:22:12 +0000

    Removed unneeded lines from Wei's script. It is currently a mystery why the lines I removed actually work with Wei's version of PyNIO. But even if they work they are unnecessary. With my version of PyNIO and on my machine they might still work eventually but they cause immense slow-down.

--- examples/write_nc4.py
+++ examples/write_nc4.py
@@ -84,22 +84,21 @@ level.units = "hPa"
 lat.units = "degrees north"
 lon.units = "degrees east"
 temp.units = "K"
-#setattr(fmdl1.variables['temp'], 'units', 'K')
 
 #fill in variables.
 time[:] = [0.0, 12.0, 24.0, 36.0, 48.0]
 level[:] =  [1000, 850, 700, 500, 300, 250, 200, 150, 100, 50]
 latvalues = numpy.arange(-90, 91, 2.5, 'float32')
 lonvalues = numpy.arange(-180, 180, 2.5, 'float32')
-#print('lat =\n',latvalues[:])
+print('lat =\n',latvalues[:])
 #print('lon =\n',lonvalues[:])
 
 #print('time shape = ', time.shape)
 #print('level shape = ', level.shape)
 tshape = time.shape
 lshape = level.shape
-#print('time shape = ', tshape[0])
-#print('level shape = ', lshape[0])
+print('time shape = ', tshape[0])
+print('level shape = ', lshape[0])
 
 #fmdl1.set_dimension("time", tshape[0])
 #fmdl1.set_dimension("level", lshape[0])
@@ -115,6 +114,7 @@ ftempvalues = numpy.ndarray(shape=(ntimes, nlevels, nlats, nlons), dtype=numpy.f
 #ftempvalues = tempvalues
 
 print('ftemp dtype = ', ftempvalues.dtype)
+
 ftempvalues[:,:,:,:] = tempvalues[:,:,:,:]
 
 print('ftemp shape = ', ftempvalues.shape)
@@ -122,16 +122,11 @@ print('ftemp dtype = ', ftempvalues.dtype)
 print "max: %f, min: %f" %(numpy.amax(tempvalues), numpy.amin(tempvalues))
 print "max: %f, min: %f" %(numpy.amax(ftempvalues), numpy.amin(ftempvalues))
 
-#fmdl1.variables['time'][:] = time
-#fmdl1.variables['level'][:] = level
-fmdl1.variables['time'].assign_value(time)
-fmdl1.variables['level'].assign_value(level)
-fmdl1.variables['lat'][:] = latvalues
-fmdl1.variables['lon'][:] = lonvalues
-#fmdl1.variables['lat'].assign_value(latvalues)
-#fmdl1.variables['lon'].assign_value(lonvalues)
-fmdl1.variables['temp'][:,:,:,:] = ftempvalues
-#fmdl1.variables['temp'].assign_value(ftempvalues)
+print fmdl1.variables['time'][:]
+print "writing lat"
+lat[:] = latvalues
+lon[:] = lonvalues
+temp[:] = ftempvalues
 
 #test data types
 nx = 2
@@ -141,64 +136,49 @@ amdl1.create_dimension("x", nx)
 #Create some variables.
 vdbl = amdl1.create_variable("vdbl", "d", ("x",))
 vdbl[:] = numpy.array([0.1234456789, 1.2345678901], numpy.double)
-amdl1.variables['vdbl'][:] = vdbl
 
 vflt = amdl1.create_variable("vflt", "f", ("x",))
 vflt[:] = [0.1234456, 1.234567]
-amdl1.variables['vflt'][:] = vdbl
 
 vint64 = amdl1.create_variable("vint64", "q", ("x",))
 vint64[:] = numpy.array([-12344567890, 12345678901234], numpy.int64)
-amdl1.variables['vint64'][:] = vint64
 
 vuint64 = amdl1.create_variable("vuint64", "Q", ("x",))
 vuint64[:] = numpy.array([1234456789, 1234567890123], numpy.uint64)
-amdl1.variables['vuint64'][:] = vuint64
 
 vlong = amdl1.create_variable("vlong", "l", ("x",))
 vlong[:] = numpy.array([-1234567, 7654321], numpy.int64)
-amdl1.variables['vlong'][:] = vlong
 
 vulong = amdl1.create_variable("vulong", "L", ("x",))
 vulong[:] = numpy.array([1234567, 87654321], numpy.uint64)
-amdl1.variables['vulong'][:] = vulong
 
 vint = amdl1.create_variable("vint", "i", ("x",))
 vint[:] = numpy.array([-1234456, 1234567], numpy.int32)
-amdl1.variables['vint'][:] = vint
 
 vuint = amdl1.create_variable("vuint", "I", ("x",))
 vuint[:] = numpy.array([1234456, 234567], numpy.uint32)
-amdl1.variables['vuint'][:] = vuint
 
 vshort = amdl1.create_variable("vshort", "h", ("x",))
 vshort[:] = numpy.array([-12345, 12345], numpy.int16)
-amdl1.variables['vshort'][:] = vshort
 
 vushort = amdl1.create_variable("vushort", "H", ("x",))
 vushort[:] = numpy.array([12345, 23451], numpy.uint16)
-amdl1.variables['vushort'][:] = vushort
 
 vbyte = amdl1.create_variable("vbyte", "b", ("x",))
 vbyte[:] = numpy.array([-123, 123], numpy.int8)
-amdl1.variables['vbyte'][:] = vbyte
 
 vubyte = amdl1.create_variable("vubyte", "B", ("x",))
 vubyte[:] = numpy.array([123, 234], numpy.uint8)
-amdl1.variables['vubyte'][:] = vubyte
 
 #vchar = amdl1.create_variable("vchar", "c", ("x",))
 #vchar[:] = numpy.chararray([23, 123], numpy.chararray)
 #amdl1.variables['vchar'][:] = vchar
 
 vlogical = amdl1.create_variable("vlogical", "?", ("x",))
-#vlogical[:] = numpy.array([0, 1], numpy.int32)
 vlogical[:] = [0, 1]
-amdl1.variables['vlogical'][:] = vlogical
 
 vstring = amdl1.create_variable("vstring", "S1", ("x",))
 vstring[:] = ["abcdef", "XYZ"]
-amdl1.variables['vstring'][:] = vstring
 
 file.close()
 
