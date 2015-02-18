837db0fac8aeadd45b3ef769beeae561727f8b1a
dbrown <dbrown@10fe2890-8a40-4696-8b1d-7ccbb19c398e>
2015-02-18 23:50:40 +0000

    Wei reminded me that I forgot to check in this new PyNIO helper module, a subclass of the dict type.

---
+++ niodict.py
@@ -0,0 +1,30 @@
+class nioDict (dict):
+    def __init__(self, *args, **kwargs):
+        self.update(*args, **kwargs)
+
+    def __getitem__(self, key):
+        #print 'in nioDict getitem', self.path, repr(self.topdict)
+        if key[0] == '/' and len(key) > 1:
+            val = dict.__getitem__(self, key[1:])
+        elif key.find('/') > 0 and not self.topdict is None:
+            newkey = ''.join([self.path,'/',key])
+            val = dict.__getitem__(self.topdict, newkey)
+        else:
+            val = dict.__getitem__(self, key)
+#        print 'GET', key
+        return val
+
+    def __setitem__(self, key, val):
+#        print 'SET', key, val
+        dict.__setitem__(self, key, val)
+
+    def __repr__(self):
+        dictrepr = dict.__repr__(self)
+        return '%s(%s)' % (type(self).__name__, dictrepr)
+
+    def update(self, *args, **kwargs):
+        #print 'update', args, kwargs
+        for k, v in dict(*args, **kwargs).iteritems():
+            self[k] = v
+   
+
